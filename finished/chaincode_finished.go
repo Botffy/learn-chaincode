0c6f6f26bff9815416b7b1be44dd6c8859983dca
IIC <IIC>
2017-03-30 14:15:50 +0200

    Add ChangeOwner function

--- finished/chaincode_finished.go
+++ finished/chaincode_finished.go
@@ -57,10 +57,31 @@ func (t *SimpleChaincode) Invoke(stub shim.ChaincodeStubInterface, function stri
 		return t.Init(stub, "init", args)
 	} else if function == "write" {
 		return t.write(stub, args)
+	} else if(function == "changeOwner") {
+		return t.changeOwner(stub, args)
 	}
 	fmt.Println("invoke did not find func: " + function)
 
 	return nil, errors.New("Received unknown function invocation: " + function)
+}
+
+func (t *SimpleChaincode) changeOwner(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
+	var asset, newowner string
+	var err error
+
+	fmt.Println("running changeOwner()")
+
+	if len(args) != 2 {
+		return nil, errors.New("NO, NO, NO")
+	}
+
+	asset = args[0]
+	newowner = args[1]
+	err = stub.PutState(asset, []byte(newowner))
+	if err != nil {
+		return nil, err
+	}
+	return nil, nil
 }
 
 // Query is our entry point for queries
